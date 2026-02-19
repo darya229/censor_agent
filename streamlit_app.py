3506bc2d43da35722cdb2cd882e2bb1e768d3efc
darya229 <daryadariaa229@gmail.com>
2026-02-19 13:23:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,14 +92,14 @@ def generate_pdf(markdown_content, filename, df = None):
 
 deepseek_llm = ChatDeepSeek(
     model="deepseek-chat",
-    api_key=API_DEEPSEEK,
+    api_key=st.secrets["MY_LLM"],
     temperature=1,
     streaming=True
 )
 
 deepseek_llm_not_streaming = ChatDeepSeek(
     model="deepseek-chat",
-    api_key=API_DEEPSEEK,
+    api_key=st.secrets["MY_LLM"],
     temperature=1
 )
 
@@ -278,5 +278,6 @@ if user_input:
             )
     else:
         st.warning("Пожалуйста, загрузите документ")
+
 
 
